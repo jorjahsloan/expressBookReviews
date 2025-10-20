a61697e05b38f998b8a0497010de1abe3438ffbe
jorjah <jorjah.sloan@gmail.com>
2025-10-20 08:08:16 -0400

    get by author

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -25,8 +25,11 @@ public_users.get('/isbn/:isbn',function (req, res) {
   
 // Get book details based on author
 public_users.get('/author/:author',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  const author = req.params.author;
+  const bookByAuthor = Object.values(books).filter(
+    x => x.author.toLowerCase() === author.toLowerCase()
+  )
+  res.send(JSON.stringify(bookByAuthor, null, 4));
 });
 
 // Get all books based on title
